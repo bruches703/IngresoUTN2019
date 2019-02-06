7954a162c69e6055de9cb94a1310201aca97dc1d
bruches703 <47338006+bruches703@users.noreply.github.com>
2019-02-06 16:38:59 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,17 @@
 function Mostrar()
 {
 //tomo la edad  
+    var edad;
+    
+    edad = document.getElementById("edad").value;
 
+        if (edad>17) 
+        {
+            alert ("es mayor");
+        }
+        else
+        {
+            alert ("es menor");
+        }
 
 }//FIN DE LA FUNCIÓN
